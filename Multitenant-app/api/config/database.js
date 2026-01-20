2b81d5cf341c6ec71c55026bdabcd9accd649caa
wikiwoo <wikiwoo@wikiwoos-MacBook-Pro.local>
2026-01-20 09:31:44 -0800

    uuu

--- Multitenant-app/api/config/database.js
+++ Multitenant-app/api/config/database.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected successfully to EasyHostnetcom');
+    console.log('MongoDB connected successfully to EasyHostnetdatabase');
   } catch (err) {
     console.error('MongoDB connection error:', err);
     process.exit(1);
